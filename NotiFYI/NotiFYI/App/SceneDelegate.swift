19335b9d2c01cdeb30a01db497f342887e2e13c6
youn9k <ace_lephant@naver.com>
2025-01-10 12:31:37 +0900

    chore :: 코드베이스 초기 세팅

---
+++ NotiFYI/NotiFYI/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let url = Secrets.BASE_URL else { return }
+        
+        print(url)
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+
+    }
+
+}
